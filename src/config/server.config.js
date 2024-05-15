bcf11b8fd1c5daa8503a902c727f5e9f3ce2a226
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-05-15 12:38:14 +0530

    Feat configured logging for file, console and mongo

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -5,5 +5,6 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT || 3000,
     ATLAS_DB_URL: process.env.ATLAS_DB_URL ,
-    NODE_ENV: process.env.NODE_ENV || "development"
+    NODE_ENV: process.env.NODE_ENV || "development",
+    LOGGER_DB_URL:process.env.LOGGER_DB_URL
 }
--- src/repositories/problem.repository.js
+++ src/repositories/problem.repository.js
@@ -1,3 +1,4 @@
+const logger = require('../config/logger.config');
 const NotFound = require('../errors/notFound.error');
 const {Problem} = require('../models');
 
@@ -42,6 +43,7 @@ class ProblemRepository {
         try {
             const problem = await Problem.findByIdAndDelete(id);
             if(!problem){
+                logger.error(`Problem.Repository: Problem with id: ${id} not found in the db`);
                 throw new NotFound("Problem", id);
             }
             return problem;
