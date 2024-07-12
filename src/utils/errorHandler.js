770f5783a292262b3b89da6ab4e1b269e2fb5b67
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-07-12 17:16:49 +0530

    updated

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -2,6 +2,7 @@ const BaseError = require("../errors/base.error");
 const { StatusCodes } = require('http-status-codes');
 function errorHandler(err, req, res, next) {
     if(err instanceof BaseError) {
+        console.log("Inside ErrorHAndler")
         return res.status(err.statusCode).json({
             success: false,
             message: err.message,
